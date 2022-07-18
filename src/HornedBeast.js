071e059560a7aa95e92fbaabdf34a2b4d939a3ab
rpruazol <rpruazol@gmail.com>
2022-07-17 21:35:07 -0400

    readme

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,7 +3,7 @@ import React from "react"
 export default class HornedBeast extends React.Component {
     render() {
         return (
-            <div id={this.props._id}>
+            <div id={this.props._id} class="horned-beast">
                 <h1>
                     {this.props.title}
                 </h1>
